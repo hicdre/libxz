bec714daa44ad862f549c52cba9f1366b69ad090
hicdre <hicdre@github.com>
2013-12-11 00:20:03 +0800

    impl css value from firefox

---
+++ CubeTest/css_value.cpp
@@ -0,0 +1,213 @@
+#include "stdafx.h"
+#include "css_value.h"
+
+namespace css
+{
+
+
+	Value::Value( int32 aValue, Unit aUnit )
+		: mUnit(aUnit)
+	{
+		if (aUnit == Unit_Integer || aUnit == Unit_Enumerated ||
+			aUnit == Unit_EnumColor) {
+				mValue.mInt = aValue;
+		}
+		else {
+			mUnit = Unit_Null;
+			mValue.mInt = 0;
+		}
+	}
+
+	Value::Value( float aValue, Unit aUnit )
+		: mUnit(aUnit)
+	{
+		if (Unit_Percent <= aUnit) {
+			mValue.mFloat = aValue;
+			//MOZ_ASSERT(!MOZ_DOUBLE_IS_NaN(mValue.mFloat));
+		}
+		else {
+			mUnit = Unit_Null;
+			mValue.mInt = 0;
+		}
+	}
+
+	Value::Value(const std::string& aValue, Unit aUnit)
+		: mUnit(aUnit)
+	{
+		DCHECK(UnitHasStringValue());
+		if (UnitHasStringValue()) {
+			mValue.mString = new std::string(aValue);
+			//if (MOZ_UNLIKELY(!mValue.mString)) {
+				// XXXbz not much we can do here; just make sure that our promise of a
+				// non-null mValue.mString holds for string units.
+			//	mUnit = eCSSUnit_Null;
+			//}
+		}
+		else {
+			mUnit = Unit_Null;
+			mValue.mInt = 0;
+		}
+	}
+
+	bool Value::IsEqual( Value* aOther ) const
+	{
+		if (mUnit == aOther->mUnit) 
+		{
+			//if (mUnit <= Unit_DummyInherit) {
+			//	return true;
+			//}
+			//else 
+			if (UnitHasStringValue()) {
+				return (mValue.mString->compare(*aOther->mValue.mString)
+					== 0);
+			}
+			else if ((Unit_Integer <= mUnit) && (mUnit <= Unit_EnumColor)) {
+				return mValue.mInt == aOther->mValue.mInt;
+			}
+			else if (Unit_Color == mUnit) {
+				return mValue.mColor == aOther->mValue.mColor;
+			}
+			//else if (UnitHasArrayValue()) {
+			//	return *mValue.mArray == *aOther->mValue.mArray;
+			//}
+			//else if (Unit_URL == mUnit) {
+			//	return *mValue.mURL == *aOther->mValue.mURL;
+			//}
+			//else if (Unit_Image == mUnit) {
+			//	return *mValue.mImage == *aOther->mValue.mImage;
+			//}
+			//else if (Unit_Gradient == mUnit) {
+			//	return *mValue.mGradient == *aOther->mValue.mGradient;
+			//}
+			//else if (Unit_Pair == mUnit) {
+			//	return *mValue.mPair == *aOther->mValue.mPair;
+			//}
+			//else if (Unit_Triplet == mUnit) {
+			//	return *mValue.mTriplet == *aOther->mValue.mTriplet;
+			//}
+			else if (Unit_Rect == mUnit) {
+				return mValue.mRect->IsEqual(aOther->mValue.mRect);
+			}
+			//else if (Unit_List == mUnit) {
+			//	return *mValue.mList == *aOther->mValue.mList;
+			//}
+			//else if (Unit_PairList == mUnit) {
+			//	return *mValue.mPairList == *aOther->mValue.mPairList;
+			//}
+			else {
+				return mValue.mFloat == aOther->mValue.mFloat;
+			}
+		}
+		return false;
+	}
+
+	void Value::Assign( Value* aCopy )
+	{
+		Reset();
+		//if (mUnit <= Unit_DummyInherit) {
+			// nothing to do, but put this important case first
+		//}
+		//else 
+		if (Unit_Percent <= mUnit) {
+			mValue.mFloat = aCopy->mValue.mFloat;
+			//MOZ_ASSERT(!MOZ_DOUBLE_IS_NaN(mValue.mFloat));
+		}
+		else if (UnitHasStringValue()) {
+			mValue.mString = aCopy->mValue.mString;
+			mValue.mString->AddRef();
+		}
+		else if (Unit_Integer <= mUnit && mUnit <= Unit_EnumColor) {
+			mValue.mInt = aCopy->mValue.mInt;
+		}
+		else if (Unit_Color == mUnit) {
+			mValue.mColor = aCopy->mValue.mColor;
+		}
+		//else if (UnitHasArrayValue()) {
+		//	mValue.mArray = aCopy->mValue.mArray;
+		//	mValue.mArray->AddRef();
+		//}
+		//else if (Unit_URL == mUnit) {
+		//	mValue.mURL = aCopy->mValue.mURL;
+		//	mValue.mURL->AddRef();
+		//}
+		//else if (Unit_Image == mUnit) {
+		//	mValue.mImage = aCopy->mValue.mImage;
+		//	mValue.mImage->AddRef();
+		//}
+		//else if (Unit_Gradient == mUnit) {
+		//	mValue.mGradient = aCopy->mValue.mGradient;
+		//	mValue.mGradient->AddRef();
+		//}
+		//else if (Unit_Pair == mUnit) {
+		//	mValue.mPair = aCopy->mValue.mPair;
+		//	mValue.mPair->AddRef();
+		//}
+		//else if (Unit_Triplet == mUnit) {
+		//	mValue.mTriplet = aCopy->mValue.mTriplet;
+		//	mValue.mTriplet->AddRef();
+		//}
+		else if (Unit_Rect == mUnit) {
+			mValue.mRect = aCopy->mValue.mRect;
+			mValue.mRect->AddRef();
+		}
+		//else if (Unit_List == mUnit) {
+		//	mValue.mList = aCopy->mValue.mList;
+		//	mValue.mList->AddRef();
+		//}
+		//else if (Unit_ListDep == mUnit) {
+		//	mValue.mListDependent = aCopy->mValue.mListDependent;
+		//}
+		//else if (Unit_PairList == mUnit) {
+		//	mValue.mPairList = aCopy->mValue.mPairList;
+		//	mValue.mPairList->AddRef();
+		//}
+		//else if (Unit_PairListDep == mUnit) {
+		//	mValue.mPairListDependent = aCopy->mValue.mPairListDependent;
+		//}
+		else {
+			NOTREACHED();
+		}
+	}
+
+	float Value::GetPixelLength() const
+	{
+		DCHECK(IsPixelLengthUnit());
+		switch (mUnit) {
+		case Unit_Pixel: 
+			return mValue.mFloat;
+		default:
+			NOTREACHED();
+			return 0;
+		}
+		return 0;
+	}
+
+	void Value::DoReset()
+	{
+		if (UnitHasStringValue()) {
+			mValue.mString->Release();
+		//} else if (UnitHasArrayValue()) {
+		//	mValue.mArray->Release();
+		//} else if (Unit_URL == mUnit) {
+		//	mValue.mURL->Release();
+		//} else if (Unit_Image == mUnit) {
+		//	mValue.mImage->Release();
+		//} else if (Unit_Gradient == mUnit) {
+		//	mValue.mGradient->Release();
+		//} else if (Unit_Pair == mUnit) {
+		//	mValue.mPair->Release();
+		//} else if (Unit_Triplet == mUnit) {
+		//	mValue.mTriplet->Release();
+		} else if (Unit_Rect == mUnit) {
+			mValue.mRect->Release();
+		//} else if (Unit_List == mUnit) {
+		//	mValue.mList->Release();
+		//} else if (Unit_PairList == mUnit) {
+		//	mValue.mPairList->Release();
+		}
+		mUnit = Unit_Null;
+	}
+
+	
+
+}
--- CubeTest/css_value.h
+++ CubeTest/css_value.h
@@ -1,48 +1,245 @@
 #pragma once
+#include "base/basictypes.h"
+#include "base/logging.h"
 
 namespace css
 {
+	enum Unit {
+		Unit_Null,	// (n/a) null unit, value is not specified
+		Unit_None,   // (n/a) value is none
+		Unit_Auto,	// (n/a) value is algorithmic
+		Unit_Inherit ,      // (n/a) value is inherited
+
+		Unit_String,  // (string*) a string value
+		Unit_Element,     // (string*) an element id
+		
+		Unit_Rect, // (RectValue*) rectangle (four values)
+
+		Unit_Integer,
+		Unit_Enumerated,
+		Unit_EnumColor,
+		Unit_Color,
+
+		Unit_Percent,
+		Unit_Number,
+
+		// Length units - relative
+		Unit_EM,    // (float) == current font size
+		Unit_RootEM,    // (float) == root element font size
+
+		Unit_Pixel,// (float) CSS pixel unit
+
+		Unit_Degree,// (float) 360 per circle
+	};
+
+	struct StringValue;
 	class Value
 	{
 	public:
-		//bool isColorCalss() const { return class_type_ == ColorClass;}
-		bool isPrimitiveCalss() const { return class_type_ == PrimitiveClass;}
+		Value(Unit nUnit = Unit_Null) : mUnit(nUnit){}
+		Value(int32 val, Unit nUnit);
+		Value(float val, Unit nUnit);
+		Value(const std::string& aValue, Unit aUnit);
+
+		~Value(){ Reset();};
+
+		bool IsEqual(Value* in) const;
+		void Assign(Value* in);
+
+		Unit GetUnit() const { return mUnit; }
+		bool IsLengthUnit() const { return Unit_EM <= mUnit && mUnit <= Unit_Pixel; }
+		bool IsFixedLengthUnit() const { return false; }
+		bool IsRelativeLengthUnit() const  { return Unit_EM <= mUnit && mUnit <= Unit_RootEM; }
+		bool IsPixelLengthUnit() const { return  mUnit == Unit_Pixel; }
+		bool IsAngularUnit() const  { return Unit_Degree == mUnit; }
+		bool UnitHasStringValue() const	{ 
+			return Unit_String <= mUnit && mUnit <= Unit_Element;
+		}
+		bool UnitHasArrayValue() const { return false; }
+
+		int32 GetIntValue() const
+		{
+			DCHECK(mUnit == Unit_Integer ||
+				mUnit == Unit_Enumerated ||
+				mUnit == Unit_EnumColor);
+			return mValue.mInt;
+		}
+
+		float GetPercentValue() const
+		{
+			DCHECK(mUnit == Unit_Percent);
+			return mValue.mFloat;
+		}
+
+		float GetFloatValue() const
+		{
+			//NS_ABORT_IF_FALSE(eCSSUnit_Number <= mUnit, "not a float value");
+			//MOZ_ASSERT(!MOZ_DOUBLE_IS_NaN(mValue.mFloat));
+			return mValue.mFloat;
+		}
+
+		float GetAngleValue() const
+		{
+			DCHECK(Degree == Unit_Degree);
+			return mValue.mFloat;
+		}
+
+		std::string& GetStringValue(std::string& aBuffer) const
+		{
+			DCHECK(UnitHasStringValue());
+			aBuffer = mValue.mString->buffer;
+			return aBuffer;
+		}
+
+		const char* GetStringBufferValue() const
+		{
+			DCHECK(UnitHasStringValue());
+			return mValue.mString->buffer.data();
+		}
+
+		unsigned GetColorValue() const
+		{
+			DCHECK(mUnit == Unit_Color);
+			return mValue.mInt;
+		}
+
+		float GetPixelLength() const;
+
+		void Reset()  // sets to null
+		{
+			if (mUnit != Unit_Null)
+				DoReset();
+		}
+	private:
+		void DoReset();
+	public:
+		void SetIntValue(int32 aValue, Unit aUnit);
+		void SetPercentValue(float aValue);
+		void SetFloatValue(float aValue, Unit aUnit);
+		void SetStringValue(const std::string& aValue, Unit aUnit);
+		void SetColorValue(unsigned aValue);
+		//void SetArrayValue(nsCSSValue::Array* aArray, nsCSSUnit aUnit);
+		//void SetURLValue(mozilla::css::URLValue* aURI);
+		//void SetImageValue(mozilla::css::ImageValue* aImage);
+		//void SetGradientValue(nsCSSValueGradient* aGradient);
+		//void SetPairValue(const nsCSSValuePair* aPair);
+		//void SetPairValue(const nsCSSValue& xValue, const nsCSSValue& yValue);
+		//void SetDependentListValue(nsCSSValueList* aList);
+		//void SetDependentPairListValue(nsCSSValuePairList* aList);
+		//void SetTripletValue(const nsCSSValueTriplet* aTriplet);
+		//void SetTripletValue(const nsCSSValue& xValue, const nsCSSValue& yValue, const nsCSSValue& zValue);
+		void SetAutoValue();
+		void SetInheritValue();
+		//void SetInitialValue();
+		void SetNoneValue();
+		//void SetAllValue();
+		void SetNormalValue();
+		//void SetSystemFontValue();
+		//void SetDummyValue();
+		//void SetDummyInheritValue();
 	protected:
-		enum ClassType {
-			PrimitiveClass,	
-			//ColorClass,
-		};
-
-		Value(ClassType type) : class_type_(type){}
-		~Value(){};
-
-		ClassType class_type_ : 16;
-		unsigned primitive_type_ : 16;
+		Unit mUnit;
+		union {
+			int32 mInt;
+			unsigned mColor;
+			float mFloat;
+			StringValue* mString;
+			RectValue* mRect;
+		} mValue;
+		DISALLOW_COPY_AND_ASSIGN(Value);
 	};
 
-	class PrimitiveValue : public Value
-	{
-	public:
-		enum UnitTypes {
-			CSS_UNKNOWN = 0,
-			CSS_NUMBER = 1,
-			CSS_PERCENTAGE = 2,
-			CSS_RGBCOLOR = 3,
-		};
+	struct RectValue {
+		RectValue(void);
+		~RectValue();
 
-		bool isNumber() const { return primitive_type_ == CSS_NUMBER;}
-		bool isPercentage() const { return primitive_type_ == CSS_PERCENTAGE; }
-		bool isRGBColor() const { return primitive_type_ == CSS_RGBCOLOR; }
+		//void AppendToString(nsCSSProperty aProperty, nsAString& aResult) const;
 
-		double getDoubleValue() const { return value_.num; }
-		float getFloatValue() const { return (float)value_.num; }
-		int getIntValue() const { return value_.ident; }
-		unsigned getRGBA32Value() const { return isRGBColor() ? value_.rgbcolor : 0; }
+		bool IsEqual(RectValue* aOther) const {
+			return mTop.IsEqual(&aOther->mTop) &&
+				mRight.IsEqual(&aOther->mRight) &&
+				mBottom.IsEqual(&aOther->mBottom) &&
+				mLeft.IsEqual(&aOther->mLeft);
+		}
+
+		void SetAllSidesTo(Value* aValue);
+
+		bool AllSidesEqualTo(Value* aValue) const {
+			return mTop.IsEqual(aValue) &&
+				mRight.IsEqual(aValue) &&
+				mBottom.IsEqual(aValue) &&
+				mLeft.IsEqual(aValue);
+		}
+
+		void Reset() {
+			mTop.Reset();
+			mRight.Reset();
+			mBottom.Reset();
+			mLeft.Reset();
+		}
+
+		bool HasValue() const {
+			return
+				mTop.GetUnit() != Unit_Null ||
+				mRight.GetUnit() != Unit_Null ||
+				mBottom.GetUnit() != Unit_Null ||
+				mLeft.GetUnit() != Unit_Null;
+		}
+
+		Value mTop;
+		Value mRight;
+		Value mBottom;
+		Value mLeft;
+
+		typedef Value RectValue::*side_type;
+		static const side_type sides[4];
+
+		void AddRef() {
+			if (mRefCnt == size_t(-1)) { // really want SIZE_MAX
+				//NS_WARNING("refcount overflow, leaking nsCSSValue::Array");
+				return;
+			}
+			++mRefCnt;
+			//NS_LOG_ADDREF(this, mRefCnt, "nsCSSValue::Array", sizeof(*this));
+		}
+		void Release() {
+			if (mRefCnt == size_t(-1)) { // really want SIZE_MAX
+				//NS_WARNING("refcount overflow, leaking nsCSSValue::Array");
+				return;
+			}
+			--mRefCnt;
+			//NS_LOG_RELEASE(this, mRefCnt, "nsCSSValue::Array");
+			if (mRefCnt == 0)
+				delete this;
+		}
 	private:
-		union {
-			int ident;
-			double num;
-			unsigned rgbcolor;
-		} value_;
+		size_t mRefCnt;
+	};
+
+	struct StringValue{
+		StringValue();
+		StringValue(const std::string& in);
+
+		bool IsEqual(StringValue* value) const{
+			return buffer.compare(value->buffer) == 0;
+		}
+
+		std::string buffer;
+		void AddRef() {
+			if (mRefCnt == size_t(-1)) { // really want SIZE_MAX
+				return;
+			}
+			++mRefCnt;
+		}
+		void Release() {
+			if (mRefCnt == size_t(-1)) { // really want SIZE_MAX
+				return;
+			}
+			--mRefCnt;
+			if (mRefCnt == 0)
+				delete this;
+		}
+	private:
+		size_t mRefCnt;
 	};
 }
